0b6426c7914f4c312471257c575504f23a6e4349
Conor Race <conorjat75@gmail.com>
2024-03-24 22:22:30 -0400

    Added code for assignment completion.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,7 @@
 // pull in our models. This will automatically load the index.js from that folder
 const models = require('../models');
 const { Cat } = models;
+const { Dog } = models;
 
 const hostIndex = async (req, res) => {
   let name = 'unknown';
@@ -115,6 +116,63 @@ const notFound = (req, res) => {
   });
 };
 
+
+// -- FUNCTIONS ADDED FOR MVC MODELS ASSIGNMENT --
+const createDog = async (req, res) => {
+  if (!req.body.firstname || !req.body.lastname || !req.body.breed || !req.body.age) {
+    return res.status(400).json({ error: 'FirstName, LastName, Breed and Age are all required.' });
+  }
+  
+  const dogData = {
+    name: `${req.body.firstname} ${req.body.lastname}`,
+    breed: req.body.breed,
+    age: req.body.age,
+  };
+
+  const newDog = new Dog(dogData);
+
+  try {
+    await newDog.save();
+    return res.status(201).json({
+      name: newDog.name,
+      breed: newDog.breed,
+      age: newDog.age,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Failed to create dog.'});
+  }
+};
+
+const editDog = async (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Name is required to perform incrementation.' });
+  }
+
+  try {
+    const doc = await Dog.findOneAndUpdate({name: req.body.name}, {$inc: {age: 1}}, {returnDocument: 'after'}).lean().exec();
+
+    if (!doc){
+      return res.status(404).json({ error: `No dog found with the name ${req.body.name}.`});
+    }
+
+    return res.json({ name: doc.name, breed: doc.breed, age: doc.age});
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong connecting to database.'});
+  }
+};
+
+const hostPage4 = async (req, res) => {
+  try {
+    const docs = await Dog.find({}).lean().exec();
+    return res.render('page4', {dogs: docs});
+  } catch (err) {
+    console.log(err);
+    return res.render('page4');
+  }
+};
+
 module.exports = {
   index: hostIndex,
   page1: hostPage1,
@@ -125,4 +183,7 @@ module.exports = {
   updateLast,
   searchName,
   notFound,
+  createDog,
+  editDog,
+  page4: hostPage4,
 };
